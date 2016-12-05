8ee82cf46088ad0432b04944fc2f6c9e8702c45d
jeffbuenting <jeffbuenting@gmail.com>
2016-12-05 12:55:58 -0500

    added test script
    this is a working script to test the various functions in the module.  It changes as I work on things.

---
+++ Test.ps1
@@ -0,0 +1,35 @@
+﻿function Save-Button {
+
+     Write-Host "Check1-Click, mach was.. " 
+    $CRMServer = $CRMServerInput.Text
+
+    Write-Host "Save : $CRMServer"
+
+    $Form.Close()
+}
+
+
+
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing") 
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")  #loading the necessary .net libraries (using void to suppress outpu
+
+import-module F:\GitHub\GUIForms\GuiForms.psd1 -force 
+
+$Form = New-GUIForm -Length 600 -Width 400
+
+$CRMServerInput = New-GUIFormInputBox -Form ([Ref]$Form) -x 20 -Y 50 -Length 150 -Height 20 -Title "CRMServer" -verbose
+
+$SaveButton  = { 
+    Write-Host "Check1-Click, mach was.. " 
+    $CRMServer = $CRMServerInput.Text
+    Test
+    $Form.Close()
+}
+
+New-GUIFormButton -Form $Form -X 400 -y 30 -Length 110 -Height 80 -Label 'Save' -Execute {Save-Button }
+
+
+$Form.add_Shown({$Form.Activate()})
+[void] $Form.ShowDialog()
+
+$CRMServer
