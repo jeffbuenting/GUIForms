d9c2087742458e694c7cd1f7beddc57a572fe678
jeffbuenting <jeffbuenting@gmail.com>
2016-12-21 10:36:00 -0500

    updating test

--- Test.ps1
+++ Test.ps1
@@ -16,20 +16,26 @@
 
 import-module F:\GitHub\GUIForms\GuiForms.psd1 -force 
 
-$WinLength = 600
+$WinLength = 1000
 $WinHeight = 400
 
 $Form = New-GUIForm -Title 'StratusLive Install' -Length $WinLength -Height $WinHeight
 
+# ----- Server Input
 $ServerX = 10
 $ServerY = 10
 $Servers = New-GUIFormGroupBox -Form ([Ref]$Form) -Title "Servers" -X $ServerX -Y $ServerY -width 250 -Height 100
-#$Form.Controls.Add($Servers)
 
 $CRMServerInput = New-GUIFormInputBox -Form ([ref]$Servers) -x ($ServerX+5) -Y ($ServerY+10) -Length 150 -Height 20 -Title "CRM Server" -verbose
 $SQLServerInput = New-GUIFormInputBox -Form ([Ref]$Servers) -x ($ServerX+5) -Y ($ServerY+35) -Length 150 -Height 20 -Title "SQL Server" -verbose
 $ADFSServerInput = New-GUIFormInputBox -Form ([Ref]$Servers) -x ($ServerX+5) -Y ($ServerY+60) -Length 150 -Height 20 -Title "ADFS Server" -verbose
 
+# ----- Source Input
+$Sources = New-GUIFormGroupBox -Form ([Ref]$Form) -Title "Sources" -X ($ServerX+260) -Y $ServerY -width 500 -Height 100
+
+$CRM2016ISOInput = New-GUIFormInputBox -Form ([ref]$Sources) -x (10) -Y (20) -Length 400 -Height 20 -Title "CRM 2016 ISO" -verbose
+
+# ----- Buttons
 New-GUIFormButton -Form $Form -X ($Form.ClientRectangle.Width-120) -y ($Form.ClientRectangle.Height-60) -Length 110 -Height 50 -Label 'Save' -Execute { $Form.Close() }
 
 $Form.add_Shown({$Form.Activate()})
